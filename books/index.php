470802780392c3bc321c211159b4662f3526005a
trfergusASU <trfergus@asu.edu>
2014-11-26 15:55:58 -0700

    Add files, fixed book pages

--- books/index.php
+++ books/index.php
@@ -1,46 +1,53 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/library/includes/header.php'); 
 
-$display_message = '';
-
 $pdo = db_connect();
-    $sql = 'SELECT * FROM book_attributes WHERE isbn IN  (SELECT DISTINCT(ISBN) FROM book b)';
-    $request = $pdo->prepare($sql);
-    $request->execute();
-	$result = $request->fetchAll();
+$sql = 'SELECT * FROM book_attributes WHERE isbn IN  (SELECT DISTINCT(ISBN) FROM book b)';
+$request = $pdo->prepare($sql);
+$request->execute();
+$result = $request->fetchAll();
+
 ?>
+
 <div class="container">
 	<div class="row">
-<div class="col-md-12" >
-	<h1>Books available for checkout</h1>
-	<table class="table table-striped">
-		<tr>
-			<th>Title</th>
-			<th>Author</th>
-			<th>Publisher</th>
-			<th>ISBN</th>
-			<th># Avail.</th>
-			<th>Reserve</th>
-		</tr>
-	<?php
-	foreach($result as $key=>$value): 
-		$pdo = db_connect();
-		    $sql = 'SELECT count(isbn) FROM book';
-		    $request = $pdo->prepare($sql);
-		    $request->execute();
-			$result = $request->fetchAll();
-		?>
-		<tr>
-			<td><?=$value['title']?></td>
-			<td><?=$value['author']?></td>
-			<td><?=$value['publisher']?></td>
-			<td><?=$value['isbn']?></td>
-			<td><?=?>
-			<td><a class="btn btn-success" href="/books/reserve/?id=<?=$value['isbn'];?>">Reserve</a></td>
-		</tr>
-	<?php endforeach; ?>
-	</table>
-</div>
-</div>
-</div>
+		<div class="col-md-12" >
+			<h1>Books available for checkout</h1>
+			<table class="table table-striped">
+				<tr>
+					<th>Title</th>
+					<th>Author</th>
+					<th>Publisher</th>
+					<th>ISBN</th>
+					<th># Avail.</th>
+					<th>Reserve</th>
+				</tr>
+				<?php
+				foreach($result as $key=>$value): 
+		
+					?>
+					<? 
+					$pdo = db_connect();
+					$sql = 'SELECT (SELECT count(isbn) FROM book b WHERE isbn = :isbn) - (SELECT count(isbn) FROM reservations WHERE isbn = :isbn AND CURDATE() > reservations.start_date AND CURDATE() < reservations.end_date OR CURDATE() = reservations.start_date)';
+					$request = $pdo->prepare($sql);
+					$isbn = $value['isbn'];
+					$request->execute(array(':isbn' => $isbn));
+					$result1 = $request->fetchColumn();
+					?>
+					<?php if($result1 > 0) : ?>
+					<tr>
+						<td><?=$value['title']?></td>
+						<td><?=$value['author']?></td>
+						<td><?=$value['publisher']?></td>
+						<td><?=$value['isbn']?></td>
+						<td><?=$result1?></td>
+							<td><a class="btn btn-success" href="/books/reserve/?isbn=<?=$value['isbn'];?>">Reserve</a></td> 
+						</tr>
+					<?php endif;?>	
+					<?php endforeach; ?>
+				</table>
+			</div>
+		</div>
+	</div>
 
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/library/includes/footer.php'); ?>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/library/includes/footer.php'); ?>
--- books/reserve/index.php
+++ books/reserve/index.php
@@ -3,49 +3,46 @@
 $display_message = '';
 
 $isbn= '';
-
-$isbn = $_GET['isbn']);
+$hello = '123';
+$isbn = $_GET['isbn'];
+	
+$user_id = $_SESSION['user_id'];
 
 $pdo = db_connect();
-    $sql = 'SELECT * FROM book_attributes WHERE isbn IN  (SELECT DISTINCT(ISBN) FROM book b)';
+    $sql = 'INSERT INTO `reservations`(`isbn`, `user_id`, `start_date`, `end_date`) VALUES (:isbn, :user_id, CURDATE() ,DATE_ADD(CURDATE(),INTERVAL 2 WEEK))';
     $request = $pdo->prepare($sql);
-    $request->execute();
+    $request->execute(array(':isbn' => $isbn, ':user_id' => $user_id));
 	$result = $request->fetchAll();
-	
-
-	
-	foreach($result as $key=>$value)
-	{
-		
-	}
-		
 
 ?>
 
 
-
-
+<div class="container">
 <div class="col-md center">
-	<h1>Books available for checkout</h1>
+	<h1>Congrats, you have checked out the following book!</h1>
 	<table class="table table-striped">
 		<tr>
 			<th>Title</th>
 			<th>Author</th>
 			<th>Publisher</th>
 			<th>ISBN</th>
-			<th>Reserve</th>
 		</tr>
 	<?php
+	$pdo = db_connect();
+	    $sql = 'SELECT * FROM book_attributes WHERE isbn = :isbn';
+	    $request = $pdo->prepare($sql);
+	    $request->execute(array(':isbn' => $isbn));
+		$result = $request->fetchAll();
 	foreach($result as $key=>$value): ?>
 		<tr>
 			<td><?=$value['title']?></td>
 			<td><?=$value['author']?></td>
 			<td><?=$value['publisher']?></td>
 			<td><?=$value['isbn']?></td>
-			<td><a class="btn btn-success" href="/books/reserve/?id=<?=$value['isbn'];?>">Reserve</a></td>
 		</tr>
 	<?php endforeach; ?>
 	</table>
+	<a class="btn btn-success" href="../">Back To Books!</a>
+</div>"
 </div>
-
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/library/includes/footer.php'); ?>
